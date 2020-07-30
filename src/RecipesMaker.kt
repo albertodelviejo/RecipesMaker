11253c3d34aae1da89da58f451bc55ec2c053725
albertodelviejo <albertodelviejosanchez@gmail.com>
2020-07-30 18:18:10 +0200

    Menu anadido

---
+++ src/RecipesMaker.kt
@@ -0,0 +1,21 @@
+fun main (args: Array<String>){
+
+    var entrada = 0
+
+    var menu: String = 
+    """:: Bienvenido a Recipe Maker ::
+
+
+    Selecciona la opción deseada
+    1. Hacer una receta
+    2. Ver mis recetas
+    3. Salir""".trimIndent()
+    
+    println(menu)
+
+    when (entrada){
+        is 1 ->  
+        is 2 ->
+        is 3 -> 
+    }
+}
